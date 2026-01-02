0f5e49e1a33b7598f7e66946fec24218d206af01
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-02 22:45:27 +0530

    Result_Details

---
+++ Result_Details.java
@@ -0,0 +1,13 @@
+public class Result_Details {
+    public static void main(String[] args) {
+        int marks = 95;
+        float percentage = 95.5f;
+        char grade = 'A';
+
+        System.out.println("Marks: " + marks);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+    }
+}
+
+
