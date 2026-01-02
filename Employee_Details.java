00e4e06f351d301126261fd8208b487fe6d15c2a
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-02 22:48:03 +0530

    Employee_Details

---
+++ Employee_Details.java
@@ -0,0 +1,13 @@
+class Employee_Details {
+    public static void main(String[] args) {
+        int employeeId = 101;
+        String employeeName = "Ravi";
+        double salary = 45000.75;
+        boolean isPermanent = true;
+
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Salary: " + salary);
+        System.out.println("Permanent Employee: " + isPermanent);
+    }
+}
