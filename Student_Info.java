01e4df4d9906b0fb001c031a0b25e7f4d79df316
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-02 22:44:15 +0530

    Student_Info

---
+++ Student_Info.java
@@ -0,0 +1,9 @@
+class Student_Info {
+    public static void main(String[] args) {
+        int age = 18;
+        String name = "Sahil";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
