d4b4e3e195884f29e85cd6d9aa8b848514b8e899
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-08 20:35:56 +0530

    Print_Backslash_Path

---
+++ Print_Backslash_Path.java
@@ -0,0 +1,10 @@
+public class Print_Backslash_Path {
+    public static void main(String[] args) {
+
+        // IN JAVA , THE BLACKSLASH (\) IS A SPECIAL CHARACTER.
+        // IF YOU WANT TO PRINT A LITERAL BACKSLASH :::
+        // YOU NEED TO ESCAPE IT WITH ANOTHER BACKSLASH.
+        // DOUBLING THE BACKSLASHES IS NECESSARY.
+        System.out.println("C:\\Java\\bin");
+    }
+}
