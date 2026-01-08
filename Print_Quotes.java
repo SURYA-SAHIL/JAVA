560881b15380f56e85c73fceaf87c16ec7147b21
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-08 20:21:01 +0530

    Print_Quotes

---
+++ Print_Quotes.java
@@ -0,0 +1,11 @@
+public class Print_Quotes 
+{
+    public static void main(String[] args)
+    
+    {
+        // (\"  = TELLS JAVA TO TREAT THE DOUBLE QUOTE AS A CHARACTER INSIDE THE STRING) 
+        //  (NOT AS THE END OF THE STRING)
+        System.out.println("\"JAVA\"");
+
+    }
+}
