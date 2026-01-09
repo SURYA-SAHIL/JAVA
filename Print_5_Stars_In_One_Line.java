597c20dc9d3d65911b757c3a794bb265c797b8d9
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-09 07:40:31 +0530

    Print_5_Stars_In_One_Line

---
+++ Print_5_Stars_In_One_Line.java
@@ -0,0 +1,12 @@
+public class Print_5_Stars_In_One_Line {
+    public static void main(String []args){
+        System.out.println("*****");
+        System.out.println();
+        // String.repeat() In Java Is Used To Create A New String.
+        //  By Repeating An Existing String A Specified Number Of Times.
+
+        System.out.println("*".repeat(5));
+
+    }
+
+}
