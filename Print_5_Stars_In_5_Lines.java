f6bb4c11ebba31101ba87c39d42e62508e3d94a3
SURYA-SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-09 07:49:28 +0530

    Print_5_Stars_In_5_Lines

---
+++ Print_5_Stars_In_5_Lines.java
@@ -0,0 +1,18 @@
+public class Print_5_Stars_In_5_Lines {
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+
+        // Create Some Space For Output:
+        System.out.println();
+
+        // OR:
+
+        System.out.println("*\n".repeat(5)); // (\n) Is Called A Newline Escape Sequence.
+
+
+    }
+}
