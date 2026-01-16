9b265dc8c80d835f15f6f76bde7afff1dc6b3a16
SURYA SAHIL <SURYASAHIL787@GMAIL.COM>
2026-01-16 19:00:52 +0530

    Add LeapYearCheck program to determine leap years

---
+++ LeapYearCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class LeapYearCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println("Leap year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not a leap year");
+        } else if (year % 4 == 0) {
+            System.out.println("Leap year");
+        } else {
+            System.out.println("Not a leap year");
+        }
+
+        sc.close();
+    }
+}
